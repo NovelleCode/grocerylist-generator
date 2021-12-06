daf7e9f4256de956e4b3f515fe0c0cec729491a6
Helena <helenalundstrom@hotmail.se>
2021-12-06 12:57:51 +0100

    Project setup

---
+++ src/main/java/se/iths/grocerylistgenerator/GrocerylistGeneratorApplication.java
@@ -0,0 +1,13 @@
+package se.iths.grocerylistgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrocerylistGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrocerylistGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/grocerylistgenerator/GrocerylistGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.grocerylistgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrocerylistGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
