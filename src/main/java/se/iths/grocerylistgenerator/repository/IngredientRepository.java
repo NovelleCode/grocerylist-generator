cf5ae21cb9537704d1148c06a38dcf1edde60540
Karen Batjes <batjeskaren@gmail.com>
2021-12-07 13:33:29 +0100

    Added ingredient repository with annotation and JpaRepository extension

--- src/main/java/se/iths/grocerylistgenerator/repository/IngredientRepository.java
+++ src/main/java/se/iths/grocerylistgenerator/repository/IngredientRepository.java
@@ -1,4 +1,9 @@
 package se.iths.grocerylistgenerator.repository;
 
-public class IngredientRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import se.iths.grocerylistgenerator.model.Ingredient;
+
+@Repository
+public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
 }
