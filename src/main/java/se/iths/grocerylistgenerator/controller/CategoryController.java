22784168d56f8b1014ef6d82664a7d88918d63d0
Karen Batjes <batjeskaren@gmail.com>
2021-12-07 18:45:47 +0100

    Created methods in CategoryController

--- src/main/java/se/iths/grocerylistgenerator/controller/CategoryController.java
+++ src/main/java/se/iths/grocerylistgenerator/controller/CategoryController.java
@@ -1,8 +1,12 @@
 package se.iths.grocerylistgenerator.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.iths.grocerylistgenerator.model.Category;
 import se.iths.grocerylistgenerator.service.CategoryService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("categories")
@@ -12,5 +16,20 @@ public class CategoryController {
 
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        return new ResponseEntity<>(categoryService.createCategory(category), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories() {
+        return new ResponseEntity<>(categoryService.getAllCategories(), HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable Long id) {
+        return new ResponseEntity<>(categoryService.getCategoryById(id), HttpStatus.OK);
     }
 }
