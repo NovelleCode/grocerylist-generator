32136a458f682896571d84233f05fcd52a0afcd9
Karen Batjes <batjeskaren@gmail.com>
2021-12-07 15:02:10 +0100

    Created methods in ingredient controller and service class

--- src/main/java/se/iths/grocerylistgenerator/controller/IngredientController.java
+++ src/main/java/se/iths/grocerylistgenerator/controller/IngredientController.java
@@ -1,4 +1,35 @@
 package se.iths.grocerylistgenerator.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.iths.grocerylistgenerator.model.Ingredient;
+import se.iths.grocerylistgenerator.service.IngredientService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController("ingredients")
 public class IngredientController {
+
+    private final IngredientService ingredientService;
+
+    public IngredientController(IngredientService ingredientService) {
+        this.ingredientService = ingredientService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Ingredient> createIngredient(@RequestBody Ingredient ingredient) {
+        return new ResponseEntity<>(ingredientService.createIngredient(ingredient), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Ingredient>> getAllIngredients() {
+        return new ResponseEntity<>(ingredientService.getAllIngredients(), HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Ingredient> getIngredientById(@PathVariable Long id) {
+        return new ResponseEntity<>(ingredientService.getIngredientById(id), HttpStatus.OK);
+    }
 }
--- src/main/java/se/iths/grocerylistgenerator/service/IngredientService.java
+++ src/main/java/se/iths/grocerylistgenerator/service/IngredientService.java
@@ -1,4 +1,31 @@
 package se.iths.grocerylistgenerator.service;
 
+import org.springframework.stereotype.Service;
+import se.iths.grocerylistgenerator.model.Ingredient;
+import se.iths.grocerylistgenerator.repository.IngredientRepository;
+
+import java.util.List;
+
+@Service
 public class IngredientService {
+
+    private final IngredientRepository ingredientRepository;
+
+    public IngredientService(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    public Ingredient createIngredient(Ingredient ingredient) {
+        if (ingredient.getName().isEmpty() || ingredient.getQuantity().isEmpty() || ingredient.getCategory() == null)
+            throw new RuntimeException("Incomplete ingredient");
+        return ingredientRepository.save(ingredient);
+    }
+
+    public List<Ingredient> getAllIngredients() {
+        return ingredientRepository.findAll();
+    }
+
+    public Ingredient getIngredientById(Long id) {
+        return ingredientRepository.findById(id).orElseThrow(() -> new RuntimeException("Ingredient with id " + id + "not found"));
+    }
 }
