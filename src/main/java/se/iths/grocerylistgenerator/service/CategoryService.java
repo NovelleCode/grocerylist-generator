4015625f178986d03c72594ae21d43e1a1c313da
Karen Batjes <batjeskaren@gmail.com>
2021-12-07 18:43:46 +0100

    Created methods in CategoryService

--- src/main/java/se/iths/grocerylistgenerator/service/CategoryService.java
+++ src/main/java/se/iths/grocerylistgenerator/service/CategoryService.java
@@ -1,7 +1,10 @@
 package se.iths.grocerylistgenerator.service;
 
 import org.springframework.stereotype.Service;
+import se.iths.grocerylistgenerator.model.Category;
 import se.iths.grocerylistgenerator.repository.CategoryRepository;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
@@ -10,5 +13,20 @@ public class CategoryService {
 
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
+    }
+
+    public Category createCategory(Category category) {
+        if(category.getName().isEmpty()) {
+            throw new RuntimeException("Incomplete category");
+        }
+        return categoryRepository.save(category);
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id).orElseThrow(() -> new RuntimeException("Category with id " + id + "not found"));
     }
 }
