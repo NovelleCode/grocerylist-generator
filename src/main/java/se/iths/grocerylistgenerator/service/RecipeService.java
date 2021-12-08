27520430824bc66708b5ac1c10d05f89f946c6ac
FridaJohanssons <frridaa_johansson@hotmail.com>
2021-12-08 12:22:57 +0100

    Added RecipeController Added Get and Post function for recipe.

--- src/main/java/se/iths/grocerylistgenerator/service/RecipeService.java
+++ src/main/java/se/iths/grocerylistgenerator/service/RecipeService.java
@@ -1,9 +1,29 @@
 package se.iths.grocerylistgenerator.service;
 
 import org.springframework.stereotype.Service;
+import se.iths.grocerylistgenerator.model.Recipe;
+import se.iths.grocerylistgenerator.repository.RecipeRepository;
+
+import java.util.Optional;
 
 @Service
 public class RecipeService {
 
+    private final RecipeRepository recipeRepository;
 
+    public RecipeService(RecipeRepository recipeRepository){
+        this.recipeRepository = recipeRepository;
+    }
+
+    public Recipe createRecipe(Recipe recipe){
+        return recipeRepository.save(recipe);
+    }
+
+    public Optional<Recipe> findRecipeById(Long id){
+        return recipeRepository.findById(id);
+    }
+
+    public Iterable<Recipe> findAllRecipes(){
+        return recipeRepository.findAll();
+    }
 }
