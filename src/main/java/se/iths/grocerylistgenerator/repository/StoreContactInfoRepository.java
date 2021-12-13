8b0510e9241b12ac30108f94bbcb8453bdef2c5e
suay-selay <suay_999@hotmail.com>
2021-12-13 13:19:01 +0100

    store contact info fixed

---
+++ src/main/java/se/iths/grocerylistgenerator/repository/StoreContactInfoRepository.java
@@ -0,0 +1,10 @@
+package se.iths.grocerylistgenerator.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import se.iths.grocerylistgenerator.model.StoreContactInfo;
+
+@Repository
+public interface StoreContactInfoRepository extends CrudRepository<StoreContactInfo, Long> {
+    StoreContactInfo findByName (String name);
+}
---
+++ src/main/java/se/iths/grocerylistgenerator/service/StoreContactInfoService.java
@@ -0,0 +1,33 @@
+package se.iths.grocerylistgenerator.service;
+
+import org.springframework.stereotype.Service;
+import se.iths.grocerylistgenerator.model.StoreContactInfo;
+import se.iths.grocerylistgenerator.repository.StoreContactInfoRepository;
+
+import java.util.Optional;
+
+@Service
+public class StoreContactInfoService {
+
+
+    private final StoreContactInfoRepository storeContactInfoRepository;
+
+
+    public StoreContactInfoService(StoreContactInfoRepository storeContactInfoRepository) {
+        this.storeContactInfoRepository = storeContactInfoRepository;
+    }
+
+    public StoreContactInfo createStoreContactInfo(StoreContactInfo storeContactInfo){
+        return storeContactInfoRepository.save(storeContactInfo);
+    }
+
+    public Optional<StoreContactInfo> findStoreContactInfoById(Long id) {
+        return storeContactInfoRepository.findById(id);
+    }
+
+    public Iterable<StoreContactInfo> findAllStoresContactInfo() {
+        return storeContactInfoRepository.findAll();
+    }
+
+
+}
