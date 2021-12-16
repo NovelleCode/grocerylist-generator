1d178609ff73381ad57a0614d58da979db9e5287
Helena <helenalundstrom@hotmail.se>
2021-12-16 16:16:16 +0100

    Ingredientservice: added method to check if ingredient already in database (by name)

--- src/main/java/se/iths/grocerylistgenerator/service/IngredientService.java
+++ src/main/java/se/iths/grocerylistgenerator/service/IngredientService.java
@@ -2,14 +2,17 @@ package se.iths.grocerylistgenerator.service;
 
 import org.springframework.stereotype.Service;
 import se.iths.grocerylistgenerator.dto.AddPersonDto;
+import se.iths.grocerylistgenerator.dto.CategoryDto;
 import se.iths.grocerylistgenerator.dto.IngredientDto;
 import se.iths.grocerylistgenerator.exception.BadRequestException;
 import se.iths.grocerylistgenerator.exception.EntityNotFoundException;
 import se.iths.grocerylistgenerator.mapper.IngredientMapper;
+import se.iths.grocerylistgenerator.model.Category;
 import se.iths.grocerylistgenerator.model.Ingredient;
 import se.iths.grocerylistgenerator.repository.IngredientRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class IngredientService {
@@ -24,6 +27,7 @@ public class IngredientService {
 
     public IngredientDto createIngredient(IngredientDto ingredientDto) {
         isValidIngredientDto(ingredientDto);
+        checkIngredientNotInDatabase(ingredientDto);
         return ingredientMapper.mapp(ingredientRepository.save(ingredientMapper.mapp(ingredientDto)));
     }
 
@@ -33,7 +37,17 @@ public class IngredientService {
         }
     }
 
-    //TODO: Ev lägga till en check här för att kolla så att ingrediensen inte redan finns i databasen?
+    private void checkIngredientNotInDatabase(IngredientDto ingredientDto) {
+        Optional<Ingredient> ingredient = findIngredientByName(ingredientDto.getName());
+        if (ingredient.isPresent()) {
+            throw new BadRequestException("The ingredient already exists in the database! Id: "
+                    + ingredient.get().getId() + ", Name: " + ingredient.get().getName() );
+        }
+    }
+
+    private Optional<Ingredient> findIngredientByName(String name) {
+        return ingredientRepository.findByName(name);
+    }
 
     public List<IngredientDto> getAllIngredients() {
         return ingredientMapper.mapp(ingredientRepository.findAll());
