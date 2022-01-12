4a4f17925de8fb398e7a02e89dbbfc6583b8ead0
Helena <helenalundstrom@hotmail.se>
2022-01-12 13:20:52 +0100

    Added to check for role in integration test

--- src/test/java/se/iths/grocerylistgenerator/GrocerylistGeneratorApplicationTests.java
+++ src/test/java/se/iths/grocerylistgenerator/GrocerylistGeneratorApplicationTests.java
@@ -11,11 +11,14 @@ import org.springframework.http.HttpStatus;
 import se.iths.grocerylistgenerator.dto.AddPersonDto;
 import se.iths.grocerylistgenerator.dto.PersonDto;
 import se.iths.grocerylistgenerator.entity.Person;
+import se.iths.grocerylistgenerator.entity.Role;
 import se.iths.grocerylistgenerator.exception.BadRequestException;
 import se.iths.grocerylistgenerator.repository.PersonRepository;
+import se.iths.grocerylistgenerator.repository.RoleRepository;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -31,6 +34,9 @@ class GrocerylistGeneratorApplicationTests {
     @Autowired
     PersonRepository personRepository;
 
+    @Autowired
+    RoleRepository roleRepository;
+
     @BeforeEach
     void setUp() {
 
@@ -38,9 +44,13 @@ class GrocerylistGeneratorApplicationTests {
         Person person2 = new Person("Mia", "456");
         Person person3 = new Person("Nisse", "789");
 
+        Role role = new Role("ROLE_USER");
+
         personRepository.save(person1);
         personRepository.save(person2);
         personRepository.save(person3);
+
+        roleRepository.save(role);
     }
 
     @AfterEach
@@ -54,11 +64,14 @@ class GrocerylistGeneratorApplicationTests {
         var createPersonResult = testRestTemplate.
                 postForEntity("http://localhost:" + port + "/signup", addPersonDto, PersonDto.class);
         assertThat(createPersonResult.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
         assertThat(Objects.requireNonNull(createPersonResult.getBody()).getUsername()).isEqualTo(addPersonDto.getUsername());
 
         List<Person> all = personRepository.findAll();
         assertThat(all.size()).isEqualTo(4);
 
+        Optional<Person> person = personRepository.findById(createPersonResult.getBody().getId());
+        person.ifPresent(value -> assertThat(value.getRole().getRoleName()).isEqualTo("ROLE_USER"));
     }
 
     @Test
